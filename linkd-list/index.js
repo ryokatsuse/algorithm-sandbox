7c2e809f052c546b337ca201586c56f195f316dd
Ryo Katsuse <ryokatsu0719@gmail.com>
2021-07-01 01:25:47 +0900

    feat:要素の追加まで実装

--- linkd-list/index.js
+++ linkd-list/index.js
@@ -1,2 +1,44 @@
 "use strict";
-var hogehoge = 'hogehoge';
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LinkedList = exports.LinkedListNode = void 0;
+var LinkedListNode = /** @class */ (function () {
+    function LinkedListNode(value, next) {
+        if (next === void 0) { next = null; }
+        this.value = value;
+        this.nextNode = next;
+    }
+    return LinkedListNode;
+}());
+exports.LinkedListNode = LinkedListNode;
+var LinkedList = /** @class */ (function () {
+    function LinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+    /**
+     * 要素を追加
+     *
+     * @param {T} value
+     * @return {*}
+     * @memberof LinkedList
+     */
+    LinkedList.prototype.push = function (value) {
+        var newNode = new LinkedListNode(value);
+        // If there is no head yet let's make new node a head.
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+            return this;
+        }
+        // Attach new node to the end of linked list.
+        this.tail = newNode;
+        this.tail = newNode;
+        return this;
+    };
+    return LinkedList;
+}());
+exports.LinkedList = LinkedList;
+var list = new LinkedList();
+list.push(1);
+list.push(4);
+console.log(list);
--- linkd-list/index.ts
+++ linkd-list/index.ts
@@ -1,1 +1,48 @@
-const hogehoge: string = 'hogehoge'
+export class LinkedListNode<T> {
+  value: T;
+  nextNode: LinkedListNode<T> | null;
+
+  constructor(value: T, next = null) {
+    this.value = value;
+    this.nextNode = next;
+  }
+}
+
+export class LinkedList<T> {
+  head: LinkedListNode<T> | null;
+  tail: LinkedListNode<T> | null;
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  /**
+   * 要素を追加
+   *
+   * @param {T} value
+   * @return {*} 
+   * @memberof LinkedList
+   */
+  push(value: T) {
+    const newNode = new LinkedListNode(value);
+
+    // If there is no head yet let's make new node a head.
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+
+      return this;
+    }
+
+    // Attach new node to the end of linked list.
+    this.tail = newNode;
+    this.tail = newNode;
+
+    return this;
+  }
+}
+
+const list = new LinkedList()
+list.push(1)
+list.push(4)
+console.log(list)
